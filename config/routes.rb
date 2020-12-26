882841e9d5b33c664ab31f411be82e883da3cde5
bclarke329 <bmendoza329@hotmail.com>
2020-12-25 23:02:57 -0600

    User can be created and logs in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/signup' => 'sessions#new'
-  post '/signup' => 'sessions#create'
+  get '/signup' => 'user#new'
+  post '/signup' => 'user#create'
 
 
-  resources :users
+  resources :user
   resources :product
   resources :product_reviews
 end
