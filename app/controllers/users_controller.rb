32cd214d1134e6aab820c1bdc7265619b3a61ff4
bclarke329 <bmendoza329@hotmail.com>
2021-10-29 19:51:49 -0500

    building out dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find_by_id(params[:id])
+        @routine = Routine.order("created_at").last
     end
 
     private 
